fcf52e14448e71663d8977af27bd649734c34781
javster101 <javster101@outlook.com>
2022-11-11 20:23:25 -0500

    auto

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,18 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
   -- Looks/Tools
   use 'romgrk/barbar.nvim'
   use 'nvim-lualine/lualine.nvim'
@@ -57,4 +71,7 @@ return require('packer').startup(function()
   use 'NMAC427/guess-indent.nvim'
   use 'windwp/nvim-autopairs'
 
+  if packer_boostrap then
+      require('packer').sync()
+  end
 end)
