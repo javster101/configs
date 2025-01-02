e6a07a485605b26881813a697b0255dd79278e07
Javier Coindreau <javster101@outlook.com>
2025-01-02 00:40:37 -0500

    the world changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -77,110 +77,9 @@ vim.g.gitblame_date_format = '%a %d %b %Y (%r)'
 
 vim.wo.stl = require('lspsaga.symbol.winbar'):get_bar()
 
-local luasnip = require("luasnip")
-local cmp = require('cmp')
-
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-cmp.setup({
-  formatting = {
-    format = function(entry, vim_item)
-      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-      vim_item.menu = ({
-        buffer = "[Buffer]",
-        nvim_lsp = "[LSP]",
-        luasnip = "[LuaSnip]",
-        nvim_lua = "[Lua]",
-        latex_symbols = "[LaTeX]",
-      })[entry.source.name]
-      return vim_item
-    end
-  },
-  view = {
-    entries = "custom"
-  },
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  sorting = {
-    priority_weight = 2,
-    comparators = {
-      require("copilot_cmp.comparators").prioritize,
-
-      -- Below is the default comparitor list and order for nvim-cmp
-      cmp.config.compare.offset,
-      -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-      cmp.config.compare.recently_used,
-      cmp.config.compare.locality,
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
-    },
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'copilot' },
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'treesitter' },
-    { name = 'luasnip' },
-    { name = 'buffer' }
-  })
-})
-
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
-
-local mason_path = vim.env.HOME .. '/.local/share/nvim/mason/'
-
-local general_attach = function(client, buffer)
-end
-
 local enhance_server_opts = {
   ["clangd"] = function(opts)
     opts.capabilities.offsetEncoding = 'utf-8'
-    -- opts.cmd = {
-    --   "clangd",
-    --   "--background-index",
-    --   "--suggest-missing-includes",
-    --   --  "--clang-tidy",
-    --   "--completion-style=detailed"
-    -- }
   end,
   ["lua_ls"] = function(opts)
     opts.settings = {
@@ -199,30 +98,30 @@ local enhance_server_opts = {
     end
     opts.on_attach = function(client, buffer)
       require('dap-python').setup('/usr/bin/python')
-      general_attach(client, buffer)
     end
   end,
 }
 
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true
-}
 vim.lsp.inlay_hint.enable(true)
-require'lspconfig'.gdscript.setup{}
+require 'lspconfig'.gdscript.setup {}
 require('mason-lspconfig').setup_handlers({
   function(server)
     local opts = {
-      on_attach = general_attach,
-      capabilities = capabilities
+      capabilities = {
+        textDocument = {
+          foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true
+          }
+        }
+      }
     }
 
     if enhance_server_opts[server] then
       enhance_server_opts[server](opts)
     end
 
+    opts.capabilities = require('blink.cmp').get_lsp_capabilities(opts.capabilities)
     require('lspconfig')[server].setup(opts)
   end,
   ['jdtls'] = function()
